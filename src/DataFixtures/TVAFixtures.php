9ae720fc27c70d08132a2ab6eb15873fba8e11a5
Didiou <didiou35@gmail.com>
2020-10-16 00:40:54 +0200

    Add reference in fixture

--- src/DataFixtures/TVAFixtures.php
+++ src/DataFixtures/TVAFixtures.php
@@ -16,14 +16,16 @@ class TVAFixtures extends Fixture
         $TVA20 = new TVA();
         $TVA20->setName("TVA 20")
             ->setRate(0.2);
-
+        
         $manager->persist($TVA20);
-
+        $this->setReference("TVA20", $TVA20);
+        
         $TVA5 = new TVA();
         $TVA5->setName("TVA 5.5")
-            ->setRate(0.055);
-
+        ->setRate(0.055);
+        
         $manager->persist($TVA5);
+        $this->setReference("TVA5", $TVA5);
 
         $manager->flush();
     }
