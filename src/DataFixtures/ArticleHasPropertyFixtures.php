ff6a20f567fcc280096d303119fa0190e981cd70
Didiou <didiou35@gmail.com>
2020-11-06 14:41:51 +0100

    Add multiple subCats for each article

--- src/DataFixtures/ArticleHasPropertyFixtures.php
+++ src/DataFixtures/ArticleHasPropertyFixtures.php
@@ -19,21 +19,32 @@ class ArticleHasPropertyFixtures extends Fixture implements DependentFixtureInte
             $article = $this->getReference("article$i");
 
             // On détermine une lettre au hasard (de A à J) pour la catégorie
-            $catLetter = chr(64+rand(1, 10));
+            $catLetter = chr(64 + rand(1, 10));
 
             $category = new Category();
-            $category = $this->getReference("category".$catLetter);
+            $category = $this->getReference("category" . $catLetter);
 
-            $subCategory = new SubCategory();
-            $subCategory = $this->getSubCategory($catLetter);
+            // On va donner entre 0 et 3 sous-cat au hasard parmi les 3 sous-cat disponibles
+            $numSubCats = rand(0, 3);
 
-            $properties = new ArticleHasProperty();
-            $properties
-                ->setArticle($article)
-                ->setCategory($category)
-                ->setSubCategory($subCategory);
+            // Initialisation de la boucle
+            $loop = 0;
 
-            $manager->persist($properties);
+            // On fait autant de tours que de sous-cat ($numSubCats)
+            do {
+                $subCategory = new SubCategory();
+                $subCategory = $numSubCats > 0 ? $this->getSubCategory($catLetter) : null;
+
+                $properties = new ArticleHasProperty();
+                $properties
+                    ->setArticle($article)
+                    ->setCategory($category)
+                    ->setSubCategory($subCategory);
+
+                $manager->persist($properties);
+
+                $loop++;
+            } while ($loop < $numSubCats);
         }
 
         $manager->flush();
@@ -53,15 +64,10 @@ class ArticleHasPropertyFixtures extends Fixture implements DependentFixtureInte
      *
      * @param string $catLetter
      */
-    public function getSubCategory(string $catLetter) {
-        // On détermine si on a ou pas une sous-cat
-        $subCatValue = rand(0, 3);
-        // Soit on a une sous cat (de 1 à 3), soit on n'en a pas (=>null)
-        if ($subCatValue > 0){
-            // On détermine un chiffre au hasard entre 1 et 3
-            $digit = strval(rand(1, 3));
-            return $this->getReference("subCategory".$catLetter.$digit);
-        }
-        else return null;
+    public function getSubCategory(string $catLetter)
+    {
+        // On détermine un chiffre au hasard entre 1 et 3
+        $digit = strval(rand(1, 3));
+        return $this->getReference("subCategory" . $catLetter . $digit);
     }
 }
